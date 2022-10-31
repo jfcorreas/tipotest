a73adcf074b48af008e9cabc90ee96f88d27b36b
Juanfran Correas <jfcorreas@gmail.com>
2022-10-31 20:07:50 +0100

    standardjs linter configured

--- src/services/apiClientServices.js
+++ src/services/apiClientServices.js
@@ -1,34 +1,28 @@
 const headersList = {
-    "Accept": "*/*",
-    "Content-Type": "application/json"
+  Accept: '*/*',
+  'Content-Type': 'application/json'
 }
 
-export async function fetchAPI(fetchParams) {
-    try {
-        const {
-            apiUrl,
-            path, objectId,
-            subpath, subObjectId,
-            filterParams, options
-        } = fetchParams
+export async function fetchAPI (fetchParams) {
+  const {
+    apiUrl,
+    path, objectId,
+    subpath, subObjectId,
+    filterParams, options
+  } = fetchParams
 
-        if (options && !options.headers) options.headers = headersList
+  if (options && !options.headers) options.headers = headersList
 
-        let requestUrl = `${apiUrl}/${path}`
+  let requestUrl = `${apiUrl}/${path}`
 
-        if (objectId) requestUrl += `/${objectId}`
-        if (subpath) requestUrl += `/${subpath}`
-        if (subObjectId) requestUrl += `/${subObjectId}`
-        if (filterParams) {
-            requestUrl += `?${new URLSearchParams(filterParams).toString()}`
-        }
- 
-        const response = await fetch(requestUrl, options)
-        const data = (response.status === 204)? null : await response.json()
+  if (objectId) requestUrl += `/${objectId}`
+  if (subpath) requestUrl += `/${subpath}`
+  if (subObjectId) requestUrl += `/${subObjectId}`
+  if (filterParams) {
+    requestUrl += `?${new URLSearchParams(filterParams).toString()}`
+  }
+  const response = await fetch(requestUrl, options)
+  const data = (response.status === 204) ? null : await response.json()
 
-        return data
-
-    } catch (error) {
-        throw error
-    }
+  return data
 }
